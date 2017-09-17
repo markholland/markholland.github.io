9b0efd03e8732d94c77701b734e51cbc646c004e
Mark Holland <mark@partiallogic.com>
2017-09-17 13:13:07 +0100

    404 image and stling

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,17 +3,7 @@ import styled from 'styled-components';
 
 import Hero from '../components/Hero';
 
-const Container = styled.div`
-  margin: 30px;
-`;
+import NotFound from '../../static/404.jpg';
 
 export default () =>
-  ( 
-    <div>
-      <Hero></Hero>
-      <Container>
-        <h1>NOT FOUND</h1>
-        <p>You just hit a page that doesn&apos;t exist...</p>
-      </Container>
-    </div>
-  );
+  <Hero image={NotFound} />;
